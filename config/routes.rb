dc8e4fd5e15b8346d2230166d27ce55574a42874
Yoan Ante <yoanante@gmail.com>
2019-02-04 22:52:08 -0500

    routes for vps

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :players
-      get "/players/:id/games", to: "players#get_games"
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      resources :game_players
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      resources :games
+
+  scope path: 'tictactaco' do 
+    namespace :api do
+      namespace :v1 do
+        resources :players
+        get "/players/:id/games", to: "players#get_games"
+        resources :game_players
+        resources :games
+      end
     end
   end
 end
